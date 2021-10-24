f3f2393e52ff741affe6463283d28d1a364e966e
Kristín Anna Sigurðardóttir <kas1@nemandi.ntv.is>
2021-10-24 20:38:10 +0000

    blabla

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Recursive1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+       
+        
+        {
+
+            List<int> num = new List<int>() { 1, 2, 3, 4 };
+
+            List<int> result = filter_greater_than(num, 3);
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+
+
+            List<int> filter_greater_than(List<int> lst, int n)
+            {
+                if (lst.Count == 0)
+                {
+                    return lst;
+                }
+                else
+                {
+                    if (lst[0] > n)
+                    {
+                        return lst.Where(x => x == lst[0]).ToList().Concat(filter_greater_than(lst.GetRange(1, lst.Count - 1), n)).ToList();
+                    }
+                    else
+                    {
+                        return filter_greater_than(lst.GetRange(1, lst.Count - 1), n);
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+   
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Recursive1
+{
+    class Program
+    {
+        static int Private(string[] args)
+        {
+
+
+            bool is_increasing(int[] arr, int n)
+            {
+                if (n == 1  n == 0)
+                        {
+
+                return 1;
+            }
+
+            if (arr[n - 1] < arr[n - 2])
+            {
+
+                return 0;
+            }
+
+            return is_increasing(arr, n - 1);
+
+            int[] arr = { 1, 2, 3, 4 };
+            int n = arr.Length;
+
+            if (is_increasing(arr, n) != 0)
+            {
+              Console.WriteLine("true");
+            }
+
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+            static int is_increasing(int[] arr, int n)
+                
+
+            if (n == 1  n == 0)
+                    {
+                return 1;
+            }
+
+            if (arr[n - 1] < arr[n - 2])
+            {
+                return 0;
+            }
+
+            return is_increasing(arr, n - 1);
+        
+        }
+
+    }
+}
+
+
